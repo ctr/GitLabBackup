317bc24b0664e40a4a57d280c0217e9c45f931e4
ctr <639838+ctr@users.noreply.github.com>
2020-02-25 12:25:46 +0000

    Extra logging.

--- run_backup_TEMPLATE.sh
+++ run_backup_TEMPLATE.sh
@@ -1,9 +1,13 @@
 ## DELETE THIS LINE, AND INSERT YOUR PRIVATE KEY FILENAME AND API KEY BELOW
 #!/bin/bash
-cd /home/.../GitLabBackup
+cd /home/ctr28/wbic-git-backup/GitLabBackup
+
+date "+GitLabBackup running at %Y%m%d-%H%M%S" >> /home/ctr28/wbic-git-backup/GitLabBackup/GitLabBackup.log
+echo " running as $(id)" >> /home/ctr28/wbic-git-backup/GitLabBackup/GitLabBackup.log
+
 mkdir bkup-auto-$(date +%A)
 
-export GIT_SSH_COMMAND='ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i /home/.../backup-key'
+export GIT_SSH_COMMAND='ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i /home/ctr28/wbic-git-backup/keys/wbic-backup-key'
 exec /usr/bin/python3 GitLabBackup.py --backup_dir bkup-auto-$(date +%A) -- https://gitlab.com "INSERT_API_KEY_HERE"
 
 
