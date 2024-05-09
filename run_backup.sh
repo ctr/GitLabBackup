bb61111b52cc6bb52404aeb5f49dc0ac50f50b69
ctr <639838+ctr@users.noreply.github.com>
2024-05-09 11:28:40 +0100

    Switch to storing api key in the keys folder.

--- run_backup.sh
+++ run_backup.sh
@@ -1,13 +1,28 @@
-## DELETE THIS LINE, AND INSERT YOUR PRIVATE KEY FILENAME AND API KEY BELOW
 #!/bin/bash
-cd /home/ctr28/wbic-git-backup/GitLabBackup
+# This is run from cron by /etc/cron.d/CTR-git-backup
+# Or run immediately with
+# # sudo -i -u gitbackup /data/backups/wbic-git-backup/GitLabBackup/run_backup.sh
 
-date "+GitLabBackup running at %Y%m%d-%H%M%S" >> /home/ctr28/wbic-git-backup/GitLabBackup/GitLabBackup.log
-echo " running as $(id)" >> /home/ctr28/wbic-git-backup/GitLabBackup/GitLabBackup.log
+# EDIT CONFIGURATION HERE:
+ROOTDIR=/data/backups/wbic-git-backup/GitLabBackup
+# API KEY must be stored in api.key file with limited permissions
+APIKEY=$(< $ROOTDIR/../keys/api.key)
+LOGDIR="$ROOTDIR/log"
+LOGFILE="$LOGDIR/GitLabBackup.log"
 
-mkdir bkup-auto-$(date +%A)
 
-export GIT_SSH_COMMAND='ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i /home/ctr28/wbic-git-backup/keys/wbic-backup-key'
-exec /usr/bin/python3 GitLabBackup.py --backup_dir bkup-auto-$(date +%A) -- https://gitlab.com "INSERT_API_KEY_HERE"
+cd "$ROOTDIR"
 
+[ ! -d "$LOGDIR" ] && mkdir "$LOGDIR" && echo "Log folder '$LOGDIR' created successfully."
+
+date "+GitLabBackup running at %Y%m%d-%H%M%S" >> "$LOGFILE"
+echo " running as $(id)" >> "$LOGFILE"
+
+DESTDIR="$ROOTDIR/bkup/auto-$(date +%A)"
+echo " DESTDIR=$DESTDIR" >> "$LOGFILE"
+
+[ ! -d "$DESTDIR" ] && mkdir "$ROOTDIR/bkup/auto-$(date +%A)"
+
+export GIT_SSH_COMMAND="ssh -o IdentitiesOnly=yes -o StrictHostKeyChecking=no -i $ROOTDIR/../keys/wbic-backup-key"
+exec /usr/bin/python3 GitLabBackup.py --backup_dir bkup/auto-$(date +%A) -- https://gitlab.com "$APIKEY" >> "$LOGFILE" 2>&1
 
